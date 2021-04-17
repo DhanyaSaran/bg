809866babe2fd4aee32189bf5ee150026f2effa9
DhanyaSaran <70140081+DhanyaSaran@users.noreply.github.com>
2021-04-17 20:08:40 +0530

    Add files via upload

--- form.js
+++ form.js
@@ -13,9 +13,16 @@ var Input
 
 button=createButton("Play")
 Input=createInput("Name:" )
-Input.position(displayWidth-300,displayHeight-300)
-button.position(displayWidth/2-350,displayHeight-350)
+Input.position(displayWidth/2-70,displayHeight/2-300)
+button.position(displayWidth/2,displayHeight/2-250)
+var name=Input.value()
 button.mousePressed(()=>{count=1
+    
+       var display=createElement('h3')
+       
+        display.html("Welcome "+name)
+        display.position(200,20)
+        console.log(display)
 Input.hide()  
 button.hide()
 
--- sketch.js
+++ sketch.js
@@ -3,15 +3,15 @@ var count = 0 , incre
 var question= []
 var answer= []
 var c=1
-
-
-
-
+var a1,a2,a3,a4
+var start="yes"
+var img=false
+var reply=""
 function preload(){
 player=loadImage("charter.png")
 backgroundimg=loadImage('pixel.jpg')
-
-
+a11=loadImage('15.png')
+a12=loadImage('20.png')
 
 
 }
@@ -20,9 +20,11 @@ backgroundimg=loadImage('pixel.jpg')
 
 function setup() {
   createCanvas(displayWidth-50,displayHeight-100);
+  img=false
  plyr=createSprite(50, displayHeight-200, 50, 50);
  plyr.addImage(player)
  plyr.scale=0.5
+ plyr.setCollider("rectangle",0,20,80,150)
  form= new Form()
  form.display()
 
@@ -41,7 +43,7 @@ function draw()
 {
   background(backgroundimg); 
   
-  
+  plyr.debug=true
   if (keyDown("UP_ARROW")){
 plyr.y=plyr.y-50
 
@@ -69,68 +71,89 @@ plyr.y=plyr.y-50
      text("Level 1",50,50 )
 
       }
-question.push("what is the square root of 225 ")
+question.push("What is the square root of 225 ")
 question.push("what is 2 to the power of three")
 answer=[[15,16,17,10]]
 
 
 for( var a in question)
-
 {
 
 if (count=== 1 )
 {
-
-textSize(18)
+ 
+//QUESTION DISPLAY
+textSize(20)
 textFont("calibri")
-
 fill("Black")
- text(question[count-1],300,70 )
- 
- var y=100
- var c=0
-  //for(var b=0; b<4; b++)
+text(question[count-1],400,100 )
 
- 
-    if (c===0)
-    {
-      
-    text(answer[count-1][c],300,400)
-    //y=y+Math.round(random(10,50))
-      c=1;
-    }
+if(start==="yes")
+{
+  answers()
+  start="no"
+}
+
+
+
+if(plyr.isTouching(a1) && img===false)
+{
+  console.log("1")
+  a1.addImage(a11)
+  a2.addImage(a12)
+  img=true
+  plyr.y=displayHeight-200
+  plyr.x=50
+
+}
+else if(plyr.isTouching(a1) && img===true)
+{
+  reply="right"
+   
+   
+  }
+
+ else if(plyr.isTouching(a2)|| plyr.isTouching(a3) || plyr.isTouching(a4))
+  {
     
-    if(c===1)
-    {//y=y+Math.round(random(100,200))
-      text(answer[count-1][c],220,300)
-      
-        c=2
+    reply="wrong"
+  }
 
-    }
-    if(c===2)
-    {
-      text(answer[count-1][c],570,200)
-      
-        x=3
-    }
-
-
-// }
-
-
-
-
-
-}
+  if(reply==="right")
+  {
+    text("Right answer",200,200)
+    a1.destroy()
+    a2.destroy()
+    a3.destroy()
+    a4.destroy()
+    plyr.y=displayHeight-200
+    plyr.x=50
+  }
+  else if(reply==="wrong")
+  {
+    text("Incorrect answer",200,200)
+    plyr.y=displayHeight-200
+    plyr.x=50
+  }
 
 }
 
-
-
-
-
+}
 
   drawSprites();
 
 }
 
+function answers()
+{
+   a1=createSprite(200,300)
+   a2=createSprite(700,250)
+   a3=createSprite(200,100)
+   a4=createSprite(300,600)
+   a1.debug=true
+   a2.debug=true
+   a3.debug=true
+   a4.debug=true
+  
+  return a1,a2,a3,a4
+}
